2481defd3f3e97ba3bd66eca36a2adfba0f9b138
Tyler Thames <tylerthames@bellsouth.net>
2022-05-21 17:09:10 -0500

    FounderButtons

--- FounderButtons/ViewController.swift
+++ FounderButtons/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var topFounderLabel: UILabel!
+    
+    
+    @IBOutlet var bottomFounderLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func googleButton(_ sender: Any) {
+        topFounderLabel.text = "Larry Page"
+        bottomFounderLabel.text = "Sergey Brin"
+    }
+    
+    @IBAction func rentTheRunwayButton(_ sender: Any) {
+        topFounderLabel.text = "Jennifer Hyman"
+        bottomFounderLabel.text = "Jenny Fleiss"
+    }
+    
+    @IBAction func clearButton(_ sender: Any) {
+        topFounderLabel.text = ""
+        bottomFounderLabel.text = ""
+    }
+    
+    
 }
 
